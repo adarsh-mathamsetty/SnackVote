fe01c3de19d74cbf54070aae893042ec8738e70c
adarsh-mathamsetty <36937702+adarsh-mathamsetty@users.noreply.github.com>
2018-02-28 21:33:42 -0600

    Add files via upload

---
+++ Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        View::share('message', 'Please select a snack to vote');
+    }
+
+
+
+    /*function boot()
+    {
+        Schema::defaultStringLength(191);
+    }*/
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
